2a31c72d4c2c66b05e280a75d7fa841f4f9bb780
caiooparque <caiosouza@oparque.art.br>
2015-08-12 11:27:50 -0300

    Mudanças gerais
    Mudanças gerais

--- theme/oparque/javascripts/main.js
+++ theme/oparque/javascripts/main.js
@@ -1,24 +1,20 @@
 function toggleMenu() {
     var menuSelector = $("nav");
-    var columnsSelector = $("nav .columns");
-
-    var buttonSelector = $("header .menu-button button");
+    var buttonSelector = $(".menu-button button");
 
     var openButtonClass = "button-open";
     var closeButtonClass = "button-close";
 
-    if (menuSelector.css("display") === "none" || columnsSelector.css("display") === "none") {
+    if (menuSelector.css("display") === "none") {
         /* Abrindo*/
         menuSelector.css("display", "block");
-        columnsSelector.css("display", "block");
-
+        
         buttonSelector.removeClass(openButtonClass);
         buttonSelector.addClass(closeButtonClass);
 
     } else {
         /* Fechando*/
         menuSelector.css("display", "");
-        columnsSelector.css("display", "");
 
         buttonSelector.removeClass(closeButtonClass);
         buttonSelector.addClass(openButtonClass);
